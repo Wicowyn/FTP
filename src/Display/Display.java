3926aa1d5455c61c0ee529e53fafe8d46f02c88c
yapiti <wicowyn@gmail.com>
2013-03-19 13:31:37 +0100

    Some work on FileExplorerLocal

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -1,13 +1,16 @@
 package Display;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.io.File;
 
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -19,6 +22,7 @@ public class Display extends JFrame {
 	private JTree treeLocal=new JTree(this.rootLocal);
 	private DefaultMutableTreeNode rootFTP=new DefaultMutableTreeNode();
 	private JTree treeFTP=new JTree(this.rootFTP);
+	private FileExplorerLocal expLocal=new FileExplorerLocal();
 	
 	public Display(){
 		JPanel pan=new JPanel();
@@ -33,22 +37,18 @@ public class Display extends JFrame {
 		panTree.setLayout(new BoxLayout(panTree, BoxLayout.X_AXIS));
 		panTree.add(this.treeLocal);
 		panTree.add(this.treeFTP);
+		panTree.add(this.expLocal);
 		pan.add(panTree);	
 		
 		this.treeLocal.addTreeSelectionListener(new TreeLocalListener());
 		this.treeFTP.addTreeSelectionListener(new TreeFTPListener());
-		showLocal("/home/yapiti");
+		showLocal("/home/yapiti");	
+		
 		
 	}
 	
 	private void showLocal(String path){
-		File fileRoot=new File(path);
-		this.rootLocal.removeAllChildren();
 		
-		for(File file : fileRoot.listFiles()){
-			DefaultMutableTreeNode node=new DefaultMutableTreeNode(file.getName());
-			this.rootLocal.add(node);
-		}
 		
 		
 	}
--- src/Display/FileExplorerLocal.java
+++ src/Display/FileExplorerLocal.java
@@ -1,7 +1,81 @@
 package Display;
 
-import javax.swing.JTree;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
-public class FileExplorerLocal extends JTree {
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class FileExplorerLocal extends JPanel {
+	private static final long serialVersionUID = 5791387224671240515L;
+	private DefaultListModel<String> model=new DefaultListModel<String>();
+	private JList<String> list=new JList<String>(this.model);
+	private String path;
 	
+	
+	public FileExplorerLocal(){
+		this(System.getProperty("user.home"));
+	}
+	
+	public FileExplorerLocal(String path){
+		this.list.addMouseListener(new ListenMouse());
+		this.add(new JScrollPane(this.list));
+		setPath(path);
+	}
+	
+	public void setPath(String path){
+		this.path=path;
+		
+		File fileRoot=new File(this.path);
+		this.model.clear();
+		
+		for(File file : fileRoot.listFiles()){
+			this.model.addElement(file.getName());
+		}
+	}
+	
+	private class ListenMouse implements MouseListener{
+		private int lastIndex=-1;
+		private int nbClick=-1;
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			int index=FileExplorerLocal.this.list.locationToIndex(e.getPoint());
+			if(index!=this.lastIndex) nbClick=0;
+			this.lastIndex=index;
+			this.nbClick++;
+			if(this.nbClick==2){
+				System.out.println("double clicked");
+			}
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
